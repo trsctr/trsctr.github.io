43a79f298433ca01dba3e4ea7e0eef2443754f1e
trsctr <trisector@gmail.com>
2025-02-03 14:15:51 +0200

    refactor link styling in about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,10 +4,11 @@ import Footer from './layout/Footer';
 import TextImageBox from './layout/TextImageBox'
 import useModal from './modal/useModal';
 import Header from './common/Header';
+import { cn } from '../utils/cn';
 
 const About: React.FC = ( ) => {
     const { toggleModal } = useModal();
-    const linkStyle = 'transition-all text-primary hover:text-accent me-4 md:me-12';
+    const linkStyle = 'transition-all text-primary hover:text-accent focus:text-accent';
 
     return (
     <ContentBox containerClassName='md:absolute flex left-0 top-0 md:top-1/4 md:left-32 w-full md:w-9/12 max-w-[900px]'
@@ -15,21 +16,21 @@ const About: React.FC = ( ) => {
         <Header text="Hello, my name is Otto" hasGradient/>
         <TextImageBox imageUrl='/assets/photo.jpg' imageOnRight={true}>        
             <p className="mb-2">I'm an aspiring software developer with a background in audio engineering and tech support, passionate about creative coding.</p>
-            <p>I recently completed my core studies at <a href="http://hive.fi" className="text-primary hover:text-accent transition-all" target="_blank">Hive Helsinki</a>, where I gained hands-on experience in programming with C and C++. </p>
+            <p>I recently completed my core studies at <a href="http://hive.fi" className={linkStyle} target="_blank">Hive Helsinki</a>, where I gained hands-on experience in programming with C and C++. </p>
             <p className="mt-2">I built this page so I could have some fun with React, Tailwind, and Three.js, and showcase my projects in the future.</p>
             <p className="mt-2">Hope you like what you see! Feel free to interact with that mesh, check out my work and connect with me on LinkedIn or GitHub.</p>
-            <p className="mt-2">Photo by <a href="https://www.instagram.com/diego_j_videos_and_photography/" className="text-primary hover:text-accent transition-all" target="_blank">Diego James</a>.</p>
+            <p className="mt-2">Photo by <a href="https://www.instagram.com/diego_j_videos_and_photography/" className={linkStyle} target="_blank">Diego James</a>.</p>
         </TextImageBox>
         <Footer>
         <li>
-            <a href="http://github.com/trsctr/trsctr.github.io" target="_blank" className={linkStyle}>Source code</a>
+            <a href="http://github.com/trsctr/trsctr.github.io" target="_blank" className={cn(linkStyle, 'me-4 md:me-12')}>Source code</a>
         </li>
         <li>
-            <a href="http://github.com/trsctr" target="_blank" className={linkStyle}>GitHub</a>
+            <a href="http://github.com/trsctr" target="_blank" className={cn(linkStyle, 'me-4 md:me-12')}>GitHub</a>
         </li>
 
         <li>
-            <a href="http://linkedin.com/in/otto-andelin" target="_blank" className={linkStyle}>LinkedIn</a>
+            <a href="http://linkedin.com/in/otto-andelin" target="_blank" className={cn(linkStyle, 'me-4 md:me-12')}>LinkedIn</a>
         </li>
         <li >
             <a href="#" onClick={toggleModal} className={linkStyle}>Contact</a>
