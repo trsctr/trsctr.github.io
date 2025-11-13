bc5006e25f5b79bcaafce13e66146050e6086ca4
trsctr <trisector@gmail.com>
2025-11-13 11:06:48 +0200

    update modal component documentation

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -45,9 +45,12 @@ const Modal: React.FC<ModalProps> = ({
 	const { isOpen, toggleModal } = useModal();
 	const modalRef = React.createRef<HTMLDivElement>(); // Create a ref for the modal
 
-
 	return (
 		<AnimatePresence>
+			{ /* Modal Overlay/backdrop 
+			 Only render if isOpen is true
+			 Animates opacity for fade in/out effect of the blurred modal backdrop 
+			*/}
 			{isOpen &&
 			<motion.div
 			aria-hidden={!isOpen}
@@ -56,26 +59,20 @@ const Modal: React.FC<ModalProps> = ({
 			animate={{ opacity: 1 }}  // Final state (fully visible)
 			exit={{ opacity: 0 }}     // Exit state (hidden)
 			transition={{ duration: .4  }}  // Duration for the transition
-			//onClick={toggleModal}  // Close the modal when clicking on the overlay
 		>
-			{/* Add animation:
-				Overlay fade-in/fade-out or blur transition
-			*/}
-
+			{ /* Modal Container
+			Animations for scaling and opacity when modal opens/closes
+			*/ }
 			<motion.div
 			ref={modalRef}
 			className="relative flex flex-col px-2 w-5/6 md:w-1/2 lg:w-1/3 max-w-lg sm:max-w-full max-h-full overflow-auto rounded-lg shadow bg-background"
 			initial={{ opacity: 0, scale: .1}}  // Initial state (scaled down and hidden)
 			animate={{ opacity: 1, scale: 1}}    // Final state (normal size and fully visible)
 			exit={{ opacity: 0.2, scale: .5 }}     // Exit state (scaled down and hidden)
-			transition={{ duration: .2,
-			}}        // Duration for the transition
+			transition={{ duration: .2 }}        // Duration for the transition
 			>
-				{/* Add animation:
-					Modal box entrance (e.g., slide-in, scale-up, fade-in)
-				*/}
-			
-				{/* Modal Header */}
+				{/* Modal Header
+				subtle fadein animation for the modal header*/}
 				<motion.div
 					initial={{ opacity: 0, y: 0 }}
 					animate={{ opacity: 1, y: 0 }}
@@ -85,23 +82,16 @@ const Modal: React.FC<ModalProps> = ({
 					{/* Close button */}
 					{hasCloseButton ? (<CloseModalButton onClick={toggleModal}/>) : (<></>)}
 				</motion.div>
-				{/* Modal Content */}
+				{/* Modal Content + animation 
+				subtle fade in animation for the modal content
+				*/}
 				<motion.div
 					initial={{ opacity: 0, y: 0 }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 0.4 }}
 					className='flex-grow px-3 pt-3 md:pt-0 pb-3 space-y-4 text-text'>
-					{/* Add animation:
-						Modal content entrance and exit (e.g., fade-in)
-						Optional, do you need animation for the content if the box is animated?
-					*/
-					}
 					{children}
 				</motion.div>
-				{/* Add animation:
-					Modal box exit (e.g., slide-out, scale-down, fade-out)
-				*/}	
-
 			</motion.div>
 		</motion.div>}
 		</AnimatePresence>
