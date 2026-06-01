fe3ccdfaa0e3a57ea674956ca2b8ce16986ee8d4
trsctr <trisector@gmail.com>
2026-06-01 09:22:11 +0300

    update about, add youtube link to footer

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,22 +15,23 @@ const About: React.FC = ( ) => {
         contentClassName='bg-background opacity-80 relative p-4 flex justify-center rounded-lg shadow-lg'>
         <Header text="Hello, my name is Otto" hasGradient/>
         <TextImageBox imageUrl='/assets/photo.jpg' imageOnRight>        
-            <p className="mb-2">I'm an aspiring software developer with a background in audio engineering and tech support, passionate about creative coding.</p>
-            <p>I recently completed my core studies at <a href="http://hive.fi" className={linkStyle} target="_blank">Hive Helsinki</a>, where I gained hands-on experience in programming with C and C++. </p>
-            <p className="mt-2">I built this page so I could have some fun with React, Tailwind, and Three.js, and showcase my projects in the future.</p>
-            <p className="mt-2">Hope you like what you see! Feel free to interact with that mesh, check out my work and connect with me on LinkedIn or GitHub.</p>
-            <p className="mt-2">Photo by <a href="https://www.instagram.com/diego_j_videos_and_photography/" className={linkStyle} target="_blank">Diego James</a>.</p>
+            <p className="mb-2">I'm a creative software developer with a background in music and audio engineering. I love experimenting, creative coding and building things that get put good use.</p>
+            <p>I studied coding at <a href="http://hive.fi" className={linkStyle} target="_blank">Hive Helsinki</a> (so I'm <a href="https://42network.org" className={linkStyle} target="_blank">42 network</a> alumni). I gained hands-on experience in programming with C and C++ by building various projects with strict limitations on which external libraries could be used (usually none).</p>
+            <p className="mt-2">Currently I'm building <a href="https://github.com/trsctr/viive" className={linkStyle} target="_blank">viive</a>, a creative delay plugin built with JUCE and C++. VST3 available for Windows and macOS, check out latest releases <a href="https://github.com/trsctr/viive/releases" className={linkStyle} target="_blank">here</a>.</p>
+            <p className="mt-4">Photo by <a href="https://www.instagram.com/diego_j_videos_and_photography/" className={linkStyle} target="_blank">Diego James</a>.</p>
         </TextImageBox>
         <Footer>
         <li>
-            <a href="http://github.com/trsctr/trsctr.github.io" target="_blank" className={cn(linkStyle, 'me-4 md:me-12')}>Source code</a>
+            <a href="https://github.com/trsctr/trsctr.github.io" target="_blank" className={cn(linkStyle, 'me-2 md:me-12')}>Source</a>
         </li>
         <li>
-            <a href="http://github.com/trsctr" target="_blank" className={cn(linkStyle, 'me-4 md:me-12')}>GitHub</a>
+            <a href="https://github.com/trsctr" target="_blank" className={cn(linkStyle, 'me-2 md:me-12')}>GitHub</a>
         </li>
-
         <li>
-            <a href="http://linkedin.com/in/otto-andelin" target="_blank" className={cn(linkStyle, 'me-4 md:me-12')}>LinkedIn</a>
+            <a href="https://www.linkedin.com/in/otto-andelin/" target="_blank" className={cn(linkStyle, 'me-2 md:me-12')}>LinkedIn</a>
+        </li>
+        <li>
+            <a href="https://youtube.com/@trsctr" target="_blank" className={cn(linkStyle, 'me-2 md:me-12')}>YouTube</a>
         </li>
         <li >
             <a href="#" onClick={toggleModal} className={linkStyle}>Contact</a>
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC<FooterProps> = ( { children }) => {
     return (
         <footer className="bg-transparent"> 
             <div className="mx-auto w-full pt-2 md:pt-5 lg:pt-2 md:flex md:items-center md:justify-between">
-                <ul className=" flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0">
+                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0 justify-evenly md:justify-start">
                     {children}
                 </ul>
             </div>
